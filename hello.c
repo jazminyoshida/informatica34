5595be1651ca905296176bd6150230e96015c4c7
jazminyoshida <jazminyoshida.24@gmail.com>
2026-04-24 14:57:41 +0000

    hello

--- hello.c
+++ hello.c
@@ -1,7 +1,12 @@
+//strings
 #include <stdio.h>
 
-int main(void)
-{
-printf("Hello Jazmin! \n");
+int main(void){
+    char YourName[50];
+    printf("Your Name is: ");
+    scanf("%s", &YourName)
+    printf("Hello %s \n", YoourName);
+    printf("\n");
+
 }
 
