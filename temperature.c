675f3464af9105494504abf4a99e657ca35d1c11
jazminyoshida <jazminyoshida.24@gmail.com>
2026-04-27 17:16:05 +0000

    temperature

--- temperature.c
+++ temperature.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
 int main(void){
-    
+    //puro CJNG
+    float cel;
+    float far;
+    printf("F= \n");
+    scanf("%f",&far);
+    cel =(f-32)/1.8;
+    printf("Farenheit: %f \n", far);
+    printf("celsius: %f \n", cel);
 }
