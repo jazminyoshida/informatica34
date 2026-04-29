7dc94cc0c227a2c2c3cea8e9d8ed01450354031a
jazminyoshida <jazminyoshida.24@gmail.com>
2026-04-29 17:28:38 +0000

    ...

---
+++ test/receipt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void){
+    char item [50];
+    float price;
+    int quantity;
+    printf("item \n");
+    printf("%c", &item);
+
+    printf("price \n");
+    printf("%f", &price);
+
+    printf("quantity \n");
+    printf("%d", &quantity);
+
+    float total = price*quantity;
+    printf("\n.");
+
+    printf("item: %c \n", item);
+    printf("price: %f \n", price);
+    printf("quantity: %d \n", quamtity);
+
+}
